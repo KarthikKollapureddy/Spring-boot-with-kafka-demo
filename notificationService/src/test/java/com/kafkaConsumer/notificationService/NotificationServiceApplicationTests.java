ea813f51aa7e32c3db315667f2daad9cbb520397
karthikkollapureddy <karthiksaik2000@gmail.com>
2026-04-19 02:33:31 +0530

    init consumer microservice

---
+++ notificationService/src/test/java/com/kafkaConsumer/notificationService/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaConsumer.notificationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
